642b073b798dca7c82aa1d61a18e058556c8da8c
CarmenDAM <cceballose01@educantabria.es>
2022-03-07 11:34:44 +0100

    Enunciado

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-import React from 'react';
-import './style.css';
-import Ejercicios1Ev from './components/Ejercicios1Ev';
-import Ejercicios2Ev from './components/Ejercicios2Ev';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div>
-      <h1>Desarrollo de Interfaces: Examen de recuperación</h1>
-      <p>
-        Resuelve los ejercicios propuestos. Para ello, haz un fork de este
-        repositorio desde tu cuenta de Github y sube un enlace a la tarea de
-        examen. El repositorio debe tener el nombre
-        examen-recuperacion-Nombre-Apellido, donde Nombre y Apellido son los
-        tuyos.
-      </p>
-      <Ejercicios1Ev />
-      <Ejercicios2Ev />
-    </div>
-  );
-}
+import App from "./App";
 
-export default App;
+ReactDOM.render(<App />, document.getElementById("root"));
