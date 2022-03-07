ab85328155bc62c6ae062816551293aa242df1b1
CarmenDAM <cceballose01@educantabria.es>
2022-03-07 13:23:24 +0100

    Lista Idiomas

--- src/components/ListaIdiomas.jsx
+++ src/components/ListaIdiomas.jsx
@@ -1,44 +1,21 @@
 import React from 'react'
 import { Menu, Dropdown } from 'antd';
   
-export default function ListaIdiomas() {
-  
-  return (
-    <div style={{
-      display: 'block', width: 700, padding: 30
-    }}>
-    
-      <>
-        <Dropdown
-          overlay={(
-            <Menu>
-              <Menu.Item key="0">
-                Czech
-              </Menu.Item>
-              <Menu.Item key="1">
-              German
-              </Menu.Item>
-              <Menu.Item key="2">
-              English
-              </Menu.Item>
-              <Menu.Item key="3">
-              Spanish
-              </Menu.Item>
-              <Menu.Item key="4">
-              French
-              </Menu.Item>
-              <Menu.Item key="5">
-              Portuguese
-              </Menu.Item>
-            </Menu>
-          )}
-          trigger={['click']}>
-          <a className="ant-dropdown-link" 
-             onClick={e => e.preventDefault()}>
-            Open Dropdown
-          </a>
-        </Dropdown>
-      </>
-    </div>
-  );
+class ListaIdiomas extends Component {
+  constructor(props) {
+    super(props);    this.state = {
+      idiomas: ['Czench', 'German', 'English', 'Spanish', 'French', 'Portuguese'],
+    };
+  }  
+
+  render() {
+    const { tasks } = this.state;    
+    return (
+      <ul>
+      {idiomas.map(idiomas => <li>{idiomas}</li>)}
+    </ul>
+      );
+  }
 }
+
+export default ListaIdiomas;
